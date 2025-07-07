1b23f7de720fb02b07b5a13acf4371cdac0a1093
moksh ahuja <mokshahuja11@gmail.com>
2025-07-07 23:02:01 +0530

    initial setup

---
+++ src/test/java/com/ai/tradingAssistant/TradingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.tradingAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
